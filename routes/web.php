557da6f457ac8a08ee97b739c0c48a47e983df38
Deepanshu Kumar <kdeepanshu99@gmail.com>
2024-11-29 16:40:17 +0530

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::controller(ProductController::class)->group(function () {
-    Route::get('/', 'index')->name('products.index');
+    Route::get('/', 'index');
     Route::get('/products', 'index')->name('products.index');
 
     Route::get('/products/create', 'create')->name('products.create');
